25164d41d770221bb3e1a040c5ec03bc597c4f1d
Audiomotivate <audiomotivateresumen@gmail.com>
2025-06-23 12:42:30 -0600

    Update index.js

--- api/admin/products/index.js
+++ api/admin/products/index.js
@@ -1,7 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 
 export default async function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
@@ -18,7 +17,14 @@ export default async function handler(req, res) {
       const products = await sql`
         SELECT 
           id, title, description, type, category, price, 
-          image_url as "imageUrl", download_url as "downloadUrl", 
+          CASE 
+            WHEN image_url LIKE 'https://drive.google.com/file/d/%' 
+            THEN CONCAT('https://drive.google.com/thumbnail?id=', SUBSTRING(image_url FROM 'https://drive.google.com/file/d/(.+?)/'), '&sz=w300-h400')
+            WHEN image_url LIKE 'https://lh3.googleusercontent.com/d/%' AND image_url NOT LIKE '%=w%'
+            THEN CONCAT(image_url, '=w300-h400')
+            ELSE image_url 
+          END as "imageUrl",
+          download_url as "downloadUrl", 
           preview_url as "previewUrl", duration, 
           badge, is_bestseller as "isBestseller", 
           is_new as "isNew", is_active as "isActive",
