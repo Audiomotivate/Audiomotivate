817e359cf8921c0375fc96d1bf927347740ca7a1
Audiomotivate <audiomotivateresumen@gmail.com>
2025-06-26 17:30:50 -0600

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { CartProvider } from "./providers/cart-provider";
+import App from "../client/src/App";
+import "../client/src/index.css";
+import { CartProvider } from "../client/src/providers/cart-provider";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "../client/src/lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
