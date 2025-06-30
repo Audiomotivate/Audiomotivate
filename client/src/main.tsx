d2fe7c57fd84959e84c854654abbb34cad2e00fc
Audiomotivate <audiomotivateresumen@gmail.com>
2025-06-29 19:23:11 -0600

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App-minimal";
 import "./index.css";
 
 // Ensure DOM is ready before mounting React
